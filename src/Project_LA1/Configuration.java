18f8e2a3fa4a887ca22fbf19cbaee8dbf39111ce
wujq95 <wujq1214@mails.jlu.edu.cn>
2020-02-03 10:31:00 -0500

    Initial commit

---
+++ src/Project_LA1/Configuration.java
@@ -0,0 +1,10 @@
+package Project_LA1;
+
+public class Configuration {
+
+    int K = 1024;
+    int M = 1024*K;
+    int TUPLE_SIZE = 100;
+    int BLOCK_SIZE = 4000;
+    int Memory_SIZE = 10 * M;
+}
---
+++ src/Project_LA1/Main.java
@@ -0,0 +1,4 @@
+package Project_LA1;
+
+public class Main {
+}
