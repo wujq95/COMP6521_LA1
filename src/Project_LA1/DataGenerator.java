ef414056c4e3a710e2aa324c8758b8457c1c22dd
wujq95 <wujq1214@mails.jlu.edu.cn>
2020-02-12 12:27:24 -0500

    data generator

--- src/Project_LA1/DataGenerator.java
+++ src/Project_LA1/DataGenerator.java
@@ -2,6 +2,9 @@ package Project_LA1;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class DataGenerator {
@@ -13,7 +16,7 @@ public class DataGenerator {
         fwClear.close();
         FileWriter fw  = new FileWriter(PATH,true);
         for(int i=0;i<50000;i++){
-            String str = getRandomString(10);
+            String str = stringGenerator();
             fw.write(str);
             fw.write("\n");
         }
@@ -27,6 +30,54 @@ public class DataGenerator {
         for(int i=0;i<length;i++){
             int number=random.nextInt(26);
             sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static String stringGenerator(){
+        StringBuffer sb = new StringBuffer();
+        String str="0123456789";
+        List<String> monthList = new ArrayList<>();
+        String[] month = {"01","02","03","04","05","06","07","08","09","10","11","12"};
+        monthList = Arrays.asList(month);
+        List<String> dayList = new ArrayList<>();
+        String[] day = {"01","02","03","04","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28"};
+        dayList = Arrays.asList(day);
+        Random random=new Random();
+        for(int i=0;i<8;i++){
+            int idNum=random.nextInt(str.length());
+            sb.append(str.charAt(idNum));
+        }
+        sb.append("201");
+        sb.append(str.charAt(random.nextInt(str.length())));
+        sb.append("-");
+        Integer monthId = random.nextInt(month.length);
+        sb.append(monthList.get(monthId));
+        sb.append("-");
+        Integer dayId = random.nextInt(day.length);
+        sb.append(dayList.get(dayId));
+        sb.append(getRandomString(5));
+        sb.append(" ");
+        sb.append(getRandomString(15));
+        int t=5;
+        while(t>0){
+            sb.append(" ");
+            t--;
+        }
+        String genderStr="01";
+        sb.append(genderStr.charAt(random.nextInt(2)));
+        String dptStr="0123456789";
+        for(int i=0;i<3;i++){
+            sb.append(dptStr.charAt(random.nextInt(10)));
+        }
+        String sinStr="0123456789";
+        for(int i=0;i<9;i++){
+            sb.append(sinStr.charAt(random.nextInt(10)));
+        }
+        sb.append(" ");
+        sb.append(getRandomString(35));
+        for(int i=0;i<9;i++){
+            sb.append(" ");
         }
         return sb.toString();
     }
