accfb4b90b8e31c687deef804bbd851e0dfaf43a
wujq95 <wujq1214@mails.jlu.edu.cn>
2020-02-17 21:07:16 -0500

    deal with code

--- src/Project_LA1/Main.java
+++ src/Project_LA1/Main.java
@@ -10,37 +10,29 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        //问题：
-        //1、block有没有用
-        //2、内存占满之后如何处理其他的东西
-        //3、可不可以第一个文档的剩余数据存储下来，然后第二次打开文档直接处理
 
         Phase1 phase1 = new Phase1();
         Phase2 phase2 = new Phase2();
 
-        //内存大小
+        //get memory size
         Runtime rt = Runtime.getRuntime();
         long totalMemory = rt.totalMemory();
         System.out.println ("total memory:"+ totalMemory/1024/1024+"M");
 
-
         Date date1 = new Date();
-        //clear the file
-        FileWriter fileWriter  = new FileWriter(Configuration.OUTPUT_PATH);
-        fileWriter.write("");
-        fileWriter.close();
-        phase1.start2(totalMemory);
+        //step1
+        phase1.start(totalMemory);
         Configuration.TEXT1_PATH = Configuration.TEXT2_PATH;
         Phase1.timeFlag=1;
-        phase1.start2(totalMemory);
+        phase1.start(totalMemory);
         Date date2 = new Date();
         long file_time1;
         file_time1 = date2.getTime()-date1.getTime();
         System.out.println("phase1 time:"+file_time1);
-        //第二步
-        Phase2 s = new Phase2();
+
+        //step2
         Date date3 = new Date();
-        s.start(totalMemory);
+        phase2.start(totalMemory);
         Date date4 = new Date();
         long file_time2;
         file_time2 = date4.getTime()-date3.getTime();
@@ -53,13 +45,17 @@ public class Main {
         int io_time = Phase2.IO_TIME;
         Delete_Temp();
 
-
         System.out.println("total time:"+ total_time);
         System.out.println("total lines:"+ lines);
         System.out.println("total blocks:"+ block_num);
         System.out.println("io times:"+ io_time);
     }
 
+    /**
+     * get line num
+     * @return
+     * @throws IOException
+     */
     public static int Get_Line() throws IOException{
         FileReader fr = new FileReader(Configuration.PHASE2_OUTPUT);
         BufferedReader br = new BufferedReader(fr);
@@ -69,6 +65,12 @@ public class Main {
         }
         return lines;
     }
+
+    /**
+     * get block num
+     * @return
+     * @throws IOException
+     */
     public static long Get_Block() throws IOException{
         File f= new File(Configuration.PHASE2_OUTPUT);
         long file_size = f.length();
@@ -76,6 +78,10 @@ public class Main {
         return block_num;
     }
 
+    /**
+     * delete useless files
+     * @throws IOException
+     */
     public static void Delete_Temp() throws  IOException{
         File file = new File(Configuration.TEMP_CONTENT);
         File[] content = file.listFiles();
